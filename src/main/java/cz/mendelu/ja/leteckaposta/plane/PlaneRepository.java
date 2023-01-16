9271fca262bd43fa818f919be6e709ac746b1262
David Flek <davidovkyflekovky@gmail.com>
2023-01-16 23:08:08 +0100

    edited db, other functionality

--- src/main/java/cz/mendelu/ja/leteckaposta/plane/PlaneRepository.java
+++ src/main/java/cz/mendelu/ja/leteckaposta/plane/PlaneRepository.java
@@ -1,13 +1,7 @@
 package cz.mendelu.ja.leteckaposta.plane;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
-public interface PlaneRepository extends JpaRepository<Plane, Character> {
-    @Query("SELECT p FROM planes p WHERE p.code = ?1")
-    Optional<Plane> findPlaneByCode(String code);
+public interface PlaneRepository extends JpaRepository<Plane, String> {
 }
